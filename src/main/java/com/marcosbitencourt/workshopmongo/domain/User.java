dcbe31835d8d4a0005433979d25c68de4bd71b58
marcb <marcbitenl@gmail.com>
2022-03-03 19:19:00 -0300

    Referenciando os posts do usuário

--- src/main/java/com/marcosbitencourt/workshopmongo/domain/User.java
+++ src/main/java/com/marcosbitencourt/workshopmongo/domain/User.java
@@ -1,20 +1,28 @@
 package com.marcosbitencourt.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import javax.sound.midi.Track;
+
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
-	
+
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -49,6 +57,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -65,5 +81,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
